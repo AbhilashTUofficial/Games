e9fdbbc1d070a7d2390507490080db01520bcb65
AbhilashTUofficial <abhilashstorm@gmail.com>
2021-01-20 11:37:53 +0530

    drawing discs implemented

--- Java/TowerOfHanoi-Gui/src/com/towerOfHanoi/GameLogic.java
+++ Java/TowerOfHanoi-Gui/src/com/towerOfHanoi/GameLogic.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class GameLogic extends JPanel {
     Scanner input = new Scanner(System.in);
     int[] peg1 = new int[]{3,2,1};
-    int[] peg2 = new int[3];
+    int[] peg2 = new int[]{2,0,0};
     int[] peg3 = new int[3];
     int[] diskCoors;
     boolean onGame = true;
@@ -19,8 +19,6 @@ public class GameLogic extends JPanel {
        diskMiddle mid=new diskMiddle();
        diskTop top=new diskTop();
        // Disk
-       g.setColor(Color.ORANGE);
-       g.fillRect(tow.pegAX+tow.pegWidth/2-140/2,tow.pegAY+tow.pegHeight-40,bot.diskWidth,bot.diskHeight);
        for(int i=0;i<=2;i++){
            for(int j=0;j<=2;j++){
                int diskWidth=0;
@@ -30,16 +28,18 @@ public class GameLogic extends JPanel {
                    if(diskCoors[0]==3){
                        diskWidth=bot.diskWidth;
                        diskHeight=bot.diskHeight;
+                       g.setColor(Color.magenta);
                    }
                    if(diskCoors[0]==2){
                        diskWidth=mid.diskWidth;
                        diskHeight=mid.diskHeight;
+                       g.setColor(Color.pink);
                    }
                    if(diskCoors[0]==1){
                        diskWidth=top.diskWidth;
                        diskHeight=top.diskHeight;
+                       g.setColor(Color.RED);
                    }
-                   g.setColor(Color.GREEN);
                    g.fillRect(diskCoors[1],diskCoors[2],diskWidth,diskHeight);
 
                }
@@ -47,18 +47,140 @@ public class GameLogic extends JPanel {
        }
    }
    int[] getDiskCoors(int[][] towers,int i,int j){
-       if(i==0)diskCoors[1]=200;
-       if(i==1)diskCoors[1]=400;
-       if(i==2)diskCoors[1]=300;
-       if(j==0)diskCoors[2]=100;
-       if(j==1)diskCoors[2]=200;
-       if(j==2)diskCoors[2]=300;
-       if(towers[i][j]==3)diskCoors[0]=3;
-       if(towers[i][j]==2)diskCoors[0]=2;
-       if(towers[i][j]==1)diskCoors[0]=1;
+       tower tow=new tower();
+       diskBottom bot=new diskBottom();
+       diskMiddle mid=new diskMiddle();
+       diskTop top=new diskTop();
+       /*
+       * Disk positions
+       * first bottom:
+       *    x: pegY + half of peg width -half of disk width */
+       int diskWidth=0;
+       int diskType=0;
+       int diskY=0;
 
+       // Find disk width
+       if(towers[i][j]==3){
+           diskWidth=bot.diskWidth;
+           diskType=3;
+       }else if(towers[i][j]==2){
+           diskWidth=mid.diskWidth;
+           diskType=2;
+       }else if(towers[i][j]==1){
+           diskWidth=top.diskWidth;
+           diskType=1;
+       }
+       // Find disk y position
+       if(j==0){
+           diskY=460;
+       }else if(j==1){
+           diskY=430;
+       }else if(j==2){
+           diskY=400;
+       }
+       // Find disk x position
+       int diskX=0;
+       if(i==0){
+           diskX=tow.pegAX-diskWidth/2+10;;
+       }else if(i==1){
+           diskX=tow.pegBX-diskWidth/2+10;;
+       }else if(i==2){
+           diskX=tow.pegBX-diskWidth/2+10;;
+       }
+       int x= tow.pegAX-diskWidth/2+10;
+       diskCoors= new int[]{diskType,diskX,diskY};
        return diskCoors;
    }
+
+    boolean winCheck(int[][] towers) {
+
+        if (towers[2][0] > towers[2][1] && towers[2][1] > towers[2][2] && towers[2][2] != 0) {
+            return false;
+        }
+        return true;
+    }
+    void choiceTake(Scanner input, int[][] towers) {
+        int onHand;
+        System.out.print("Take from: ");
+        int ch = input.nextInt();
+        switch (ch) {
+            case 1:
+                onHand = take(towers[ch - 1]);
+                break;
+            case 2:
+                onHand = take(towers[ch - 1]);
+                break;
+            case 3:
+                onHand = take(towers[ch - 1]);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + ch);
+        }
+        choicePlace(input, towers, onHand);
+    }
+    void choicePlace(Scanner input, int[][] towers, int onHand) {
+        System.out.print("Place to: ");
+        int ch = input.nextInt();
+        switch (ch) {
+            case 1:
+                if (check(towers[ch - 1], onHand)) place(towers[ch - 1], onHand);
+                else {
+                    System.out.println("Can't do that!");
+                    choicePlace(input, towers, onHand);
+                }
+                break;
+            case 2:
+                if (check(towers[ch - 1], onHand)) place(towers[ch - 1], onHand);
+                else {
+                    System.out.println("Can't do that!");
+                    choicePlace(input, towers, onHand);
+                }
+                break;
+            case 3:
+                if (check(towers[ch - 1], onHand)) place(towers[ch - 1], onHand);
+                else {
+
+                    System.out.println("Can't do that!");
+                    choicePlace(input, towers, onHand);
+                }
+                break;
+        }
+    }
+
+    boolean check(int[] tower, int onHand) {
+        if (tower[0] == 0) return true;
+        else if (tower[0] < onHand) return false;
+        else if (tower[0] > onHand) {
+            if (tower[1] == 0) return true;
+            else if (tower[1] < onHand) return false;
+            else if (tower[1] > onHand) {
+                if (tower[2] == 0) return true;
+            }
+        }
+        return true;
+    }
+
+    int take(int[] peg) {
+        int onHand = 0;
+        for (int i = peg.length - 1; i > -1; i--) {
+            if (peg[i] != 0) {
+                onHand = peg[i];
+                peg[i] = 0;
+                i = -1;
+            }
+        }
+        return onHand;
+    }
+    int place(int[] peg, int disk) {
+        for (int i = 0; i <= 2; i++) {
+            if (peg[i] == 0) {
+                peg[i] = disk;
+                return 0;
+            }
+        }
+        return 0;
+    }
+
 
 }
 class diskTop extends tower{
--- Java/TowerOfHanoi-Gui/src/com/towerOfHanoi/Window.java
+++ Java/TowerOfHanoi-Gui/src/com/towerOfHanoi/Window.java
@@ -2,7 +2,6 @@ package com.towerOfHanoi;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Arrays;
 
 class screen{
     int x=200;
@@ -20,29 +19,27 @@ class tower extends screen{
 
     int pegDist=200;
     int pegAX=pegDist-10;
-    int pegAY=300;
+    int pegY =300;
     int pegBX=pegDist*2-10;
-    int pegBY=300;
     int pegCX=pegDist*3-10;
-    int pegCY=300;
     Color pegColor=Color.DARK_GRAY;
     int pegWidth=20;
     int pegHeight=200;
     int pegFeetHeight=10;
     int pegFeetWidth=pegDist-10;
     int pegAFeetX=pegAX+5-(pegFeetWidth)/2;
-    int pegAFeetY=pegAY+pegHeight-pegFeetHeight;
+    int pegAFeetY= pegY +pegHeight-pegFeetHeight;
     int pegBFeetX=pegBX+5-(pegFeetWidth)/2;
-    int pegBFeetY=pegBY+pegHeight-pegFeetHeight;
+    int pegBFeetY= pegY +pegHeight-pegFeetHeight;
     int pegCFeetX=pegCX+5-(pegFeetWidth)/2;
-    int pegCFeetY=pegCY+pegHeight-pegFeetHeight;
+    int pegCFeetY= pegY +pegHeight-pegFeetHeight;
     int pegTipRadius=pegWidth+1;
     int pegATipX=pegAX-1;
-    int pegATipY=pegAY-pegTipRadius/2;
+    int pegATipY= pegY -pegTipRadius/2;
     int pegBTipX=pegBX-1;
-    int pegBTipY=pegBY-pegTipRadius/2;
+    int pegBTipY= pegY -pegTipRadius/2;
     int pegCTipX=pegCX-1;
-    int pegCTipY=pegCY-pegTipRadius/2;
+    int pegCTipY= pegY -pegTipRadius/2;
 }
 
 
@@ -70,20 +67,17 @@ public class Window extends JFrame {
     }
     public void drawPegs(Graphics g){
         tower tow=new tower();
-        diskTop top=new diskTop();
-        diskMiddle mid=new diskMiddle();
-        diskBottom bot=new diskBottom();
         g.setColor(tow.pegColor);
         // Peg A
-        g.fillRect(tow.pegAX,tow.pegAY,tow.pegWidth,tow.pegHeight);
+        g.fillRect(tow.pegAX,tow.pegY,tow.pegWidth,tow.pegHeight);
         g.fillRect(tow.pegAFeetX,tow.pegAFeetY,tow.pegFeetWidth,tow.pegFeetHeight);
         g.fillOval(tow.pegATipX,tow.pegATipY,tow.pegTipRadius,tow.pegTipRadius);
         // Peg B
-        g.fillRect(tow.pegBX,tow.pegBY,tow.pegWidth,tow.pegHeight);
+        g.fillRect(tow.pegBX,tow.pegY,tow.pegWidth,tow.pegHeight);
         g.fillRect(tow.pegBFeetX,tow.pegBFeetY,tow.pegFeetWidth,tow.pegFeetHeight);
         g.fillOval(tow.pegBTipX,tow.pegBTipY,tow.pegTipRadius,tow.pegTipRadius);
         // Peg C
-        g.fillRect(tow.pegCX,tow.pegCY,tow.pegWidth,tow.pegHeight);
+        g.fillRect(tow.pegCX,tow.pegY,tow.pegWidth,tow.pegHeight);
         g.fillRect(tow.pegCFeetX,tow.pegCFeetY,tow.pegFeetWidth,tow.pegFeetHeight);
         g.fillOval(tow.pegCTipX,tow.pegCTipY,tow.pegTipRadius,tow.pegTipRadius);
     }
