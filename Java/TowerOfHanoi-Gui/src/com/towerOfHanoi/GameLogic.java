05b51b279f837a5866db1d155496b5e69cf5981f
AbhilashTUoffical <abhilashstorm@gmail.com>
2021-01-27 16:27:35 +0530

    update

--- Java/TowerOfHanoi-Gui/src/com/towerOfHanoi/GameLogic.java
+++ Java/TowerOfHanoi-Gui/src/com/towerOfHanoi/GameLogic.java
@@ -2,17 +2,17 @@ package com.towerOfHanoi;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class GameLogic extends JPanel {
     Scanner input = new Scanner(System.in);
     int[] peg1 = new int[]{3,2,1};
-    int[] peg2 = new int[]{2,0,0};
-    int[] peg3 = new int[3];
+    int[] peg2 = new int[]{0,0,2};
+    int[] peg3 = new int[]{0,0,0};
     int[] diskCoors;
     boolean onGame = true;
     int[][] towers = {peg1, peg2, peg3};
+// Drawing section
    void drawDisk(Graphics g){
        tower tow=new tower();
        diskBottom bot =new diskBottom();
